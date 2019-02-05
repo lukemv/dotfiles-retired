a7e11e333dec1f32152c15a31a4c914128155962
Luke M <luke@lukemuccillo.com>
2019-02-06 05:18:55 +1100

    History changes and nvm

--- bashrc.d/export.bash
+++ bashrc.d/export.bash
@@ -1,7 +1,10 @@
 
-export HISTCONTROL=ignoreboth
+export HISTCONTROL=ignoredups:erasedups:ignoreboth
 export HISTSIZE=1000
 export HISTFILESIZE=2000
+
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
@@ -19,5 +22,11 @@ function returncode
 
 export GOPATH="$HOME/code/go"
 export PATH=$PATH:$GOPATH/bin
+export PATH="$PATH:/home/lukem/.local/bin"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 set -o vi
+
