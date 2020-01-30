aadbc3531148aabda434816890b7b93dc2b3f36d
Luke M <luke@lukemuccillo.com>
2020-01-30 19:20:51 +1100

    Prompt tweaks

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -11,10 +11,10 @@ parse_git_branch ()
   then
     gitver=${c_green}'('$gitver')'${c_sgr0}
   else
-    gitver=${c_red}'!('$gitver')'${c_sgr0}
+    gitver=${c_red}'('$gitver')'${c_sgr0}
   fi
   else
-  return 0
+    return 0
   fi
   echo $gitver
 }
@@ -22,7 +22,7 @@ parse_git_branch ()
 
 promptCommand() {
   if [ $? -eq 0 ]; then
-    ERRPROMPT=" "
+    ERRPROMPT=""
     HAS_ERROR=0
   else
     ERRPROMPT=" ($?) "
@@ -63,37 +63,40 @@ promptCommand() {
     CURENT_PATH="...$(echo -n $PWD | sed -e "s/\(^.\{$CUT\}\)\(.*\)/\2/")"
   fi
 
-  local TITLEBAR="\[\e]2;${CURENT_PATH}\a"
+  local DASH="${STATUS_COLOR}═${DEFAULT}"
+  local TC=" ╔"
+  local BC="═╩"
+
+  local BLOCK_START="${STATUS_COLOR}[${DEFAULT}"
+  local BLOCK_END="${STATUS_COLOR}]${DEFAULT}"
 
   local VENV=''
+  local SUFFIX=''
   # Display python virtualenv
   if [[ $VIRTUAL_ENV != "" ]]
     then
-    VENV=" ${RED}(${VIRTUAL_ENV##*/})"
+    VENV="${CYAN}[${VIRTUAL_ENV##*/}]"
+    SUFFIX=${DASH}${VENV} 
+  fi
+
+  if [[ $BRANCH != "" ]]
+    then
+    SUFFIX="${SUFFIX} ${BRANCH}"
+  fi
+
+  if [[ $ERRPROMPT != "" ]]
+    then
+    SUFFIX="${SUFFIX} ${STATUS_COLOR}${ERRPROMPT}"
   fi
 
   # different prompt and color for root
   local PR="$ "
   local USERNAME_COLORED="${WHITE}${USER} @ ${WHITE}${HOSTNAME_SHORT}"
-  if [ "$UID" = "0" ]; then
-    echo "I am root"
-    PR="# " 
-    USERNAME_COLORED="${RED}${USER}${GREEN}@${RED}${HOSTNAME_SHORT}"
-  fi
-
   # use only ASCII symbols in linux console
-  local DASH="-"
-  local TC=" ╔"
-  local BC="═╩"
-  if [ "$TERM" = "linux" ]; then
-    TITLEBAR=""
-    DASH="-"
-    TC=""
-    BC=""
-  fi
-
-  local TOP_LINE="${STATUS_COLOR}${TC}${CYAN}[ ${USERNAME_COLORED} ${GREEN}]${DASH}${GREEN}[ ${WHITE}${TIME}${GREEN} ]${DASH}${GREEN}[ ${GRAY}${CURENT_PATH} ${GREEN}] ${RED}${VENV} ${GREEN}${BRANCH} ${YELLOW}$ERRPROMPT"
-  local BOTTOM_LINE="${STATUS_COLOR}${BC}${CYAN}[ ${GREEN}${PR}${DEFAULT}"
+  
+  #${DASH}${RED}${VENV}${DASH}${GREEN}${BRANCH}${DASH}${YELLOW}$ERRPROMPT
+  local TOP_LINE="${STATUS_COLOR}${TC}${CYAN}${BLOCK_START} ${USERNAME_COLORED} ${BLOCK_END}${DASH}${BLOCK_START} ${WHITE}${TIME} ${BLOCK_END}${DASH}${BLOCK_START} ${GRAY}${CURENT_PATH} ${BLOCK_END}${SUFFIX}"
+  local BOTTOM_LINE="${STATUS_COLOR}${BC}${CYAN}${BLOCK_START} ${GREEN}${PR}${DEFAULT}"
   export PS1="${TOP_LINE}\n${BOTTOM_LINE}"
 }
 PROMPT_COMMAND=promptCommand
