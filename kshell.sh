7c9e21e1da04f058310109c19e8009349c5e1abe
Luke M <luke@lukemuccillo.com>
2020-02-25 17:51:49 +1100

    add kshell script

---
+++ kshell.sh
@@ -0,0 +1,5 @@
+files=( bashrc tmux.conf vimrc vim gitconfig bashrc.d scripts )
+for file in ${files[@]}; do
+	ln -sfn "$(pwd)/${file}" "$HOME/.${file}" && echo "simlink created .$file"
+done
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
