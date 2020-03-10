e08f5b5d43e6c6f474ce1739aabdb828fbc09030
Luke M <luke@lukemuccillo.com>
2020-03-10 16:00:06 +1100

    Add absolute path for links in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 files=( bashrc tmux.conf vimrc vim gitconfig bashrc.d scripts )
 for file in ${files[@]}; do
-	ln -sfn "$(pwd)/${file}" "$HOME/.${file}" && echo "simlink created .$file"
+	ln -sfn "$HOME/dotfiles/${file}" "$HOME/.${file}" && echo "simlink created .$file"
 done
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
