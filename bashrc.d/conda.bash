53c2057bf60e65ceca831e974cac9d727a63d4d3
Luke M <luke@lukemuccillo.com>
2021-01-05 19:15:08 +0000

    Force miniconda bin path

--- bashrc.d/conda.bash
+++ bashrc.d/conda.bash
@@ -2,13 +2,14 @@
 # >>> conda initialize >>>
 __conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
+    echo "Evaluating conda setup variable"
     eval "$__conda_setup"
 else
+    echo "Doing the other thing"
     if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
         . "$HOME/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="$HOME/miniconda3/bin:$PATH"
     fi
+    export PATH="$HOME/miniconda3/bin:$PATH"
 fi
 unset __conda_setup
 # <<< conda initialize <<<
